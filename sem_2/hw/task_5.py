ad45eeb9a626a611c52aa613c4464c9e4b19f5b1
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-10-06 23:52:12 +0500

    sem 3 inclass tasks

--- sem_2/hw/task_5.py
+++ sem_2/hw/task_5.py
@@ -2,11 +2,18 @@
 # 10
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
+from hashlib import new
 from random import random
 
 arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-arr[0] = int(random() * 10)
 
-random()
+
+for i in range(10):
+    temp_rnd = round(random() * 10)
+    if temp_rnd == arr[i]:
+        continue
+    else:
+        arr[i] = temp_rnd
+
 
 print(arr)
