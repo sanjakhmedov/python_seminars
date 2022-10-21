15e89b264163922b03f93858f0a3cec83451b2a2
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-10-22 01:45:12 +0500

    training repetition

--- sem_3/classwork/task_1.py
+++ sem_3/classwork/task_1.py
@@ -1,18 +1,41 @@
 import random
 
-size = int(input('enter the value for size of list: '))
-num = int(input('enter the number: '))
+# size = int(input('enter the value for size of list: '))
+# num = int(input('enter the number: '))
+#
+#
+# def check_num(s, n):
+#     if n < 0:
+#         return print('Error')
+#     ls = random.sample(range(s * 2), s)
+#     print(ls)
+#
+#     if n in ls:
+#         return print('YES')
+#     return print('NO')
+#
+#
+# check_num(size, num)
+
+list_length = int(input('Indicate the list length: '))
+num = int(input('Enter the number: '))
 
 
-def check_num(s, n):
-    if n < 0:
-        return print('Error')
-    ls = random.sample(range(s * 2), s)
-    print(ls)
-
-    if n in ls:
-        return print('YES')
-    return print('NO')
+def create_list(size):
+    if size <= 0:
+        return 'Error'
+    nums = random.sample(range(size * 2), size)
+    return nums
 
 
-check_num(size, num)
+def is_num_in_list(n_list, n):
+    if n in n_list:
+        return 'Yes'
+    return 'No'
+
+
+ls = create_list(list_length)
+print(ls)
+result = is_num_in_list(ls, num)
+print(result)
+
--- sem_3/classwork/task_2.py
+++ sem_3/classwork/task_2.py
@@ -2,31 +2,62 @@
 # Напишите программу, которая определит индекс второго вхождения строки в списке
 # либо сообщит, что её нет.
 
+import random
 from random import choices
 
 
-def create_rnd_ls(count1, source1):
-    result = []
-    for i in range(count1):
-        temp = choices(source1, k=3)
-        result.append(''.join(temp))
-    return result
+# def create_rnd_ls(count1, source1):
+#     result = []
+#     for i in range(count1):
+#         temp = choices(source1, k=3)
+#         result.append(''.join(temp))
+#     return result
+#
+#
+#
+# def find_second_intercourse(word1, words_list1):
+#     if words_list1.count(word1) > 1:
+#         first_ent = words_list1.index(word1)
+#         print(f'second intercourse: {words_list1.index(word1, first_ent + 1)}')
+#     else:
+#         print('-1')
+#
+#
+#
+#
+# count, source = int(input('enter the count value: ')), input('enter the source value: ')
+#
+# words = create_rnd_ls(count, source)
+# print(words)
+#
+# find_second_intercourse(input(), words)
 
 
+size = int(input("Enter the list length: "))
+item = input("Enter the letters without spaces: ")
 
-def find_second_intercourse(word1, words_list1):
-    if words_list1.count(word1) > 1:
-        first_ent = words_list1.index(word1)
-        print(f'second intercourse: {words_list1.index(word1, first_ent + 1)}')
+
+def create_list(s, item1):
+    ls = []
+    for i in range(s):
+        temp = choices(item1, k=3)
+        ls.append("".join(temp))
+    return ls
+
+
+rnd_list = create_list(size, item)
+search = input("Enter the search item: ")
+
+
+def check_second_intercourse(ls, word):
+    if ls.count(word) > 1:
+        ent_one = ls.index(word)
+        print(f"Second intercourse: {ls.index(word, ent_one + 1)}")
     else:
-        print('-1')
+        print(-1)
 
 
+print(rnd_list)
+check_second_intercourse(rnd_list, search)
 
 
-count, source = int(input('enter the count value: ')), input('enter the source value: ')
-
-words = create_rnd_ls(count, source)
-print(words)
-
-find_second_intercourse(input(), words)
